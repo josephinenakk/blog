2e659ce257744e13399bda230d74fd7d80765b9a
josephinenakk <josephine.nakka@gmail.com>
2015-04-21 11:12:03 -0400

    blog app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417173500) do
+ActiveRecord::Schema.define(version: 20150420160701) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20150417173500) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "hashed_password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
